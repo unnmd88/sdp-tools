42431f542918cd5992ff9670c504544b6605133f
auser <unnmd88@gmail.com>
2025-10-27 01:02:38 +0300

    add user model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -4,8 +4,23 @@ from sqlalchemy import UniqueConstraint
 from core.models import Base
 
 
+# class User(Base):
+#     username: Mapped[str] = mapped_column(unique=True)
+#     # foo: Mapped[int]
+#     # bar: Mapped[int]
+#     #
+#     # __table_args__ = (
+#     #     UniqueConstraint('foo', 'bar'),
+#     # )
+
 class User(Base):
+
     username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(unique=True)
+    is_active: Mapped[bool]
+    is_admin: Mapped[bool]
+
     # foo: Mapped[int]
     # bar: Mapped[int]
     #
