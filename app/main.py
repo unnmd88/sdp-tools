0c704b1813cecb662d01992242303e51ec70e1f1
auserw <unnmd88@gmail.com>
2025-10-29 18:40:24 +0300

    corr routes

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import uvicorn
 
 from app.core.config import settings
 from fastapi import FastAPI
-from users.demo import router as demo_auth_router
+# from users.demo import router as demo_auth_router
 from api import router as api_v1_router
 
 
 app = FastAPI()
 app.include_router(router=api_v1_router)
-app.include_router(router=demo_auth_router, prefix=settings.api.v1.prefix)
+# app.include_router(router=demo_auth_router, prefix=settings.api.v1.prefix)
 
 
 @app.get('/')
