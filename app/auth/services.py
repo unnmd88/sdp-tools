e65a3e8f8ac010e192b9c1179028f311b6b2b364
auserw <unnmd88@gmail.com>
2025-10-30 15:39:50 +0300

    refactor exceptions

--- app/auth/services.py
+++ app/auth/services.py
@@ -1,22 +1,14 @@
-from fastapi import Form, HTTPException
-from sqlalchemy import select
-from starlette import status
+from fastapi import Form
 
 from auth import utils as auth_utils
+from auth.exceptions import AuthenticationError, InactiveUserError
 from auth.schemas import UserSchema
-from core.models import User, db_api
 from users.crud import get_user_by_username_or_none
-
-unauthed_exc = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED, detail='invalid username or password'
-)
 
 
 def check_user_is_active(user_is_active: bool) -> bool:
     if not user_is_active:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail='inactive user'
-        )
+        raise InactiveUserError
     return True
 
 
@@ -24,7 +16,7 @@ def check_password_is_valid(plain_password: str, hashed_password: bytes) -> bool
     if not auth_utils.validate_password(
         password=plain_password, hashed_password=hashed_password
     ):
-        raise unauthed_exc
+        raise AuthenticationError
     return True
 
 
@@ -33,7 +25,7 @@ async def validate_auth_user(
     password: str = Form(),
 ):
     if (user := await get_user_by_username_or_none(username)) is None:
-        raise unauthed_exc
+        raise AuthenticationError
     check_password_is_valid(password, user.password)
     check_user_is_active(user.is_active)
     return UserSchema.model_validate(user, from_attributes=True)
