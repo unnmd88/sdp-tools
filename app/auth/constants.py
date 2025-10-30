5546a590e2bb368dbdeb6233aaf9030ff2a11e4f
auserw <unnmd88@gmail.com>
2025-10-30 15:35:27 +0300

    add constants

--- app/auth/constants.py
+++ app/auth/constants.py
@@ -3,6 +3,7 @@ from enum import StrEnum
 
 class TokenFields(StrEnum):
     pk_id = 'id'
+    user_id = 'user_id'
     username = 'username'
     sub = 'sub'
     exp = 'exp'
@@ -13,3 +14,9 @@ class TokenFields(StrEnum):
     is_active = 'is_active'
     is_superuser = 'is_superuser'
     organization = 'organization'
+    typ = 'typ'
+
+
+class TokenTypes(StrEnum):
+    access = 'access'
+    refresh = 'refresh'
