efa2a433b94f1b4a9e4d6d95205d43a99348bed7
auser <unnmd88@gmail.com>
2025-11-02 00:55:24 +0300

    add tests

--- app/tests/unit/users/test_user.py
+++ app/tests/unit/users/test_user.py
@@ -1,12 +1,20 @@
-from auth.token_validation import check_is_active_superuser
-from tests.fixtures.fixtures_user import user_schemas
+import asyncio
+
 import pytest
 
+from fastapi import HTTPException
+from auth.token_validation import check_is_active_superuser
+from tests.fixtures.fixtures_user import user_schemas
 
-# @pytest.mark.asyncio
+
 async def test_check_is_active_superuser(user_schemas):
-    for user in user_schemas:
-        assert await check_is_active_superuser(user)
-    # assert all(await check_is_active_superuser(u) for u in user_schemas)
+    users = await asyncio.gather(*[check_is_active_superuser(u) for u in user_schemas])
+    assert all(user.is_superuser for user in users)
+    for user in users:
+        user.is_superuser = False
+
+    with pytest.raises(HTTPException):
+        users = await asyncio.gather(*[check_is_active_superuser(u) for u in user_schemas])
+
 
 
