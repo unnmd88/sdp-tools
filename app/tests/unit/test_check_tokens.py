737735fdce0a877b746ea578383b8cc2f8bb4b42
auser <unnmd88@gmail.com>
2025-11-02 01:58:40 +0300

    add tests

---
+++ app/tests/unit/test_check_tokens.py
@@ -0,0 +1,22 @@
+import pytest
+from contextlib import nullcontext
+
+from fastapi import HTTPException
+
+from auth.constants import TokenTypes
+from auth.token_validation import check_token_type
+
+
+@pytest.mark.parametrize(
+    ('token_type', 'expected_token_type', 'expected_res'),
+    [
+        (TokenTypes.access, TokenTypes.access, nullcontext()),
+        (TokenTypes.refresh, TokenTypes.refresh, nullcontext()),
+        (TokenTypes.access, TokenTypes.refresh, pytest.raises(HTTPException)),
+        (TokenTypes.refresh, TokenTypes.access, pytest.raises(HTTPException)),
+    ]
+)
+def test_check_token_type(token_type, expected_token_type, expected_res):
+    with expected_res:
+        assert check_token_type(token_type, expected_token_type)
+
