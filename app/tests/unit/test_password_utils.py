10d5f8a55d5176de12edde56e1c6889f7519aca0
auserw <unnmd88@gmail.com>
2025-11-04 14:35:45 +0300

    add tests

--- app/tests/unit/test_password_utils.py
+++ app/tests/unit/test_password_utils.py
@@ -1,11 +1,23 @@
+import random
+import string
+
 import pytest
 
-from auth.utils import hash_password
+from auth.utils import hash_password, validate_password, gen_password
 
 from contextlib import nullcontext
 
 
 class TestPasswordUtils:
+
+    @pytest.fixture
+    def passwords(self) -> list[str]:
+        return  [gen_password() for _ in range(4)]
+
+    def test_gen_password(self):
+        for passwd_length in range(10, 21):
+            _passwd = gen_password(min_length=passwd_length, max_length=passwd_length)
+            assert passwd_length == len(_passwd) and isinstance(_passwd, str)
 
     @pytest.mark.parametrize(
         ('password', 'expected'),
@@ -22,3 +34,16 @@ class TestPasswordUtils:
         with expected:
             # assert isinstance(password, str)
             assert isinstance(hash_password(password), bytes)
+
+    def test_validate_password_true(self, passwords):
+        for passwd in passwords:
+            hashed_passwd =  hash_password(passwd)
+            assert validate_password(passwd, hashed_passwd)
+
+    def test_validate_password_false(self, passwords):
+        chars = string.ascii_letters + string.digits + string.punctuation
+        for passwd in passwords:
+            hashed_passwd =  hash_password(passwd)
+            chars_added_to_passwd = "".join(random.choices(chars, k=random.randint(1, 10)))
+            mut_passwd = passwd + chars_added_to_passwd
+            assert not validate_password(mut_passwd, hashed_passwd)
