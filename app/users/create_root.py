273392cf921209e6aef9768dddd9101f6c5c7fbe
auserw <unnmd88@gmail.com>
2025-11-04 17:09:46 +0300

    refactor create_root(). it is allowed to create a user only with a script. previously, it was possible with an http request

--- app/users/create_root.py
+++ app/users/create_root.py
@@ -1,21 +1,32 @@
 import asyncio
+import logging
 
-from core.models import db_api
+import bcrypt
 
-from users import crud as crud_users
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app_logging.dev.config import USERS_LOGGER
+from core.models import db_api, User
+
 from users.organizations import Organizations
 from users.roles import Roles
-from users.schemas import CreateUser
 
 
-async def create_root():
+logger = logging.getLogger(USERS_LOGGER)
+
+
+async def create_root(session: AsyncSession = None):
     name = 'root'
-    password = 'sdp2025'
-    user = CreateUser(
+    password = bcrypt.hashpw(
+        'sdp2025'.encode('utf-8'),
+        bcrypt.gensalt(),
+    )
+    user_root = User(
         first_name=name,
         last_name=name,
         username=name,
-        organization=Organizations.spetsdorproject,
+        organization=Organizations.SDP,
         password=password,
         is_active=True,
         is_admin=True,
@@ -25,10 +36,19 @@ async def create_root():
         telegram='',
         description='',
     )
-    async with db_api.session_factory() as session:
-        res = await crud_users.create_user(user=user, sess=session, from_app=True)
-
-    return res
+    sess: AsyncSession = session or db_api.session_factory()
+    try:
+        sess.add(user_root)
+        await sess.commit()
+        await sess.refresh(user_root)
+        logger.info('User root was created with id=%s.', user_root.id)
+    except IntegrityError:
+        logger.warning('User root already exists.')
+    except AssertionError as e:
+        logger.critical('Fatal error. User root was not created: %s', e)
+        raise
+    finally:
+        await sess.aclose()
 
 
 if __name__ == '__main__':
