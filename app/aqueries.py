ba09cd35e560543778133a59e7f618c6effa86b1
auserw <unnmd88@gmail.com>
2025-11-05 12:20:03 +0300

    add query

--- app/aqueries.py
+++ app/aqueries.py
@@ -1,20 +1,54 @@
 import asyncio
 
-from sqlalchemy.sql.expression import update
+from sqlalchemy import Result
+from sqlalchemy.sql.expression import update, select
 
 from core.models import db_api, User
+from core.models.database_api import DatabaseAPI
 
 
-async def main():
+# def t_dp_api():
+#     return DatabaseAPI(
+#         url='postgresql+asyncpg://admin2:1111@localhost:5433/db_test',
+#         echo=True,
+#         echo_pool=True,
+#         pool_size=50,
+#         max_overflow=10,
+#     )
 
+
+t_dp_api = DatabaseAPI(
+        url='postgresql+asyncpg://admin2:1111@localhost:5433/db_test',
+        echo=True,
+        echo_pool=True,
+        pool_size=50,
+        max_overflow=10,
+    )
+
+
+async def update_telegram():
     async with db_api.session_factory() as session:
         stmt = (
-        update(User)
-        .where(User.id == 1)
-        .values(telegram="@patrick")
+            update(User)
+            .where(User.id == 1)
+            .values(telegram="@patrick")
         )
         await session.execute(stmt)
         await session.commit()
+
+
+async def search():
+
+    async with t_dp_api.session_factory() as session:
+        stmt = select(User).where(User.username.in_(['test1', 'test2']))
+        result: Result = await session.execute(stmt)
+        print(result.scalars().all())
+
+
+async def main():
+    await search()
+
+
 
 
 if __name__ == '__main__':
