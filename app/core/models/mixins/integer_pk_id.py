77ccaaf5c253172aad5c3fb1b87ba62897949aa5
auserw <unnmd88@gmail.com>
2025-11-06 19:15:01 +0300

    fix IntegerIdPkMixin,  add autoincrement=True

--- app/core/models/mixins/integer_pk_id.py
+++ app/core/models/mixins/integer_pk_id.py
@@ -2,4 +2,4 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class IntegerIdPkMixin:
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
