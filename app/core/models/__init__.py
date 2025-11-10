37caf04696a4edb25ee55a9b70cc267437bc7532
auserw <unnmd88@gmail.com>
2025-11-10 17:54:18 +0300

    add models

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -2,8 +2,12 @@ __all__ = (
     'Base',
     'User',
     'TrafficLightObject',
+    'Region',
+    'OvimPassport',
 )
 
 from .base import Base
 from .tlo import TrafficLightObject
 from .user import User
+from .regions import Region
+from .ovim_passport import OvimPassport
