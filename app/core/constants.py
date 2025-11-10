d9b22a6e016168067b694b834fc2b4eee35b2017
auserw <unnmd88@gmail.com>
2025-11-10 17:50:56 +0300

    add constants for traffic-lights-regions

---
+++ app/core/constants.py
@@ -0,0 +1,11 @@
+from enum import IntEnum, StrEnum
+
+
+class RegionCodes(IntEnum):
+    MOSCOW = 77
+    TMUTARAKAN = 1111
+
+
+class RegionNames(StrEnum):
+    MOSCOW = "Москва"
+    TMUTARAKAN = "Тмутаракань"
