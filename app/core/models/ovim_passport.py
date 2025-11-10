109f2b3339141b1412accfbeb669338b4a3ec476
auserw <unnmd88@gmail.com>
2025-11-10 17:50:21 +0300

    add new model OvimPassport

---
+++ app/core/models/ovim_passport.py
@@ -0,0 +1,27 @@
+from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy.dialects.postgresql.json import JSONB
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+from core.models.mixins.integer_pk_id import IntegerIdPkMixin
+from core.models.mixins.timestamp import CreatedAtMixin
+
+
+class OvimPassport(
+    IntegerIdPkMixin,
+    CreatedAtMixin,
+    Base,
+):
+    tlo_id: Mapped[int] = mapped_column(
+        ForeignKey("traffic_light_objects.id"),
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+
+    )
+    data = mapped_column(
+        JSONB,
+        nullable=False,
+    )
+    commit_message: Mapped[str] = mapped_column(Text)
+
