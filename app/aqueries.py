333fe4588354809bdf1d14a24450e5ff500eb22f
auserw <unnmd88@gmail.com>
2025-11-10 17:54:41 +0300

    add some queries

--- app/aqueries.py
+++ app/aqueries.py
@@ -3,7 +3,8 @@ import asyncio
 from sqlalchemy import Result
 from sqlalchemy.sql.expression import update, select
 
-from core.models import User, TrafficLightObject
+from core.database import db_api as db_api_main
+from core.models import User, TrafficLightObject, Region, OvimPassport
 from core.database.api import DatabaseAPI
 
 
@@ -87,24 +88,72 @@ async def create_users():
         session.add_all([User(**u) for u in users])
 
 
-async def create_traffic_light_objects():
+async def create_traffic_light_objects(
+    db_api: DatabaseAPI,
+    region_id=1,
+    name='laba_test',
+    district='ЦАО',
+    street='BAZA BEREG'
+):
     objects = [
         TrafficLightObject(
-            region=1,
-            name='laba_test',
-            district='ЦАО',
-            street='BAZA BEREG'
-
+            region_id=region_id,
+            name=name,
+            district=district,
+            street=street,
+        ),
+        TrafficLightObject(
+            region_id=region_id,
+            name='laba_test2',
+            district=district,
+            street='BAZA BEREG2',
         )
     ]
-    async for session in t_dp_api.session_getter_commit():
+    async for session in db_api.session_getter_commit():
+        session.add_all(objects)
+
+
+async def create_region(
+    db_api: DatabaseAPI,
+    num: int = 77,
+    name = 'Москва',
+
+):
+    objects = [
+        Region(code=num, name=name),
+    ]
+
+    async for session in db_api.session_getter_commit():
+        session.add_all(objects)
+
+
+async def create_ovim_passport(
+    db_api: DatabaseAPI,
+    tlo_id=1,
+    user_id=1,
+    data: dict = None,
+    commit_message='commit tets',
+):
+    objects = [
+        OvimPassport(
+            tlo_id=tlo_id,
+            data=data or {"Test1": 1, "Test2": 2, 55: "abra"},
+            user_id=user_id,
+            commit_message=commit_message,
+        )
+    ]
+    async for session in db_api.session_getter_commit():
         session.add_all(objects)
 
 
 async def main():
     # await search()
     # await create_users()
-    await create_traffic_light_objects()
+    # await create_traffic_light_objects(db_api=db_api_main)
+
+    # await create_region(db_api=db_api_main)
+    # await create_traffic_light_objects(db_api=db_api_main)
+    await create_ovim_passport(db_api=db_api_main, tlo_id=2,)
 
 
 if __name__ == '__main__':
