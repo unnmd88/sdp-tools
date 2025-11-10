44f298b0410c7e9eba58c1d4d25250aafd450958
auserw <unnmd88@gmail.com>
2025-11-10 17:54:02 +0300

    add schemas for Region

---
+++ app/api/api_v1/regions/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, ConfigDict, AfterValidator
+from sqlalchemy.sql.annotation import Annotated
+
+from core.constants import RegionCodes, RegionNames
+
+
+class CreateRegionSchema(BaseModel):
+
+    name: RegionNames
+    code: RegionCodes
+
+class RegionSchema(CreateRegionSchema):
+    id: int
