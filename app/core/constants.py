706e90c0eb1e08569c80af4039fbcb8dfddb1a56
auserw <unnmd88@gmail.com>
2025-11-11 08:04:40 +0300

    add constants

--- app/core/constants.py
+++ app/core/constants.py
@@ -3,11 +3,15 @@ from enum import IntEnum, StrEnum
 
 class RegionCodes(IntEnum):
     MOSCOW77 = 77
-    MOSCOW177 = 177
-    TMUTARAKAN = 1111
+    TVER = 69
+    SAKHALIN = 65
+    RYBINSK = 76
+    TMUTARAKAN1111 = 1111
 
 
 class RegionNames(StrEnum):
     MOSCOW = 'Москва'
-    MOSCOW2 = 'Москва2'
+    TVER = 'Тверь'
+    SAKHALIN = 'Сахалин'
+    RYBINSK = 'Рыбинск'
     TMUTARAKAN = 'Тмутаракань'
