4fb3c2acab6f597ed8f122fad86f42ae8fd2d01b
auserw <unnmd88@gmail.com>
2025-11-11 18:04:27 +0300

    include router: passports_router

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -4,9 +4,11 @@ from fastapi import APIRouter
 from users.routes import router as users_router
 from .tlo.routes import router as tlo_router
 from .regions.routes import router as regions_router
+from .passports.routes import router as passports_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
 router.include_router(auth_router)
 router.include_router(users_router)
 router.include_router(tlo_router)
 router.include_router(regions_router)
+router.include_router(passports_router)
