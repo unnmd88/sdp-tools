e4dac8b096f3426914944a131d9602fc792581f6
auserw <unnmd88@gmail.com>
2025-11-11 18:00:52 +0300

    add repr for model OvimPassport

--- app/core/models/ovim_passport.py
+++ app/core/models/ovim_passport.py
@@ -1,10 +1,16 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey, String, Text
 from sqlalchemy.dialects.postgresql.json import JSONB
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
 from core.models.mixins.integer_pk_id import IntegerIdPkMixin
 from core.models.mixins.timestamp import CreatedAtMixin
+
+if TYPE_CHECKING:
+    from . import User
+    from . import TrafficLightObject
 
 
 class OvimPassport(
@@ -23,3 +29,21 @@ class OvimPassport(
         nullable=False,
     )
     commit_message: Mapped[str] = mapped_column(Text)
+
+
+    # tlo: Mapped["TrafficLightObject"] = relationship(
+    #     lazy="joined",
+    # )
+    # user: Mapped["User"] = relationship(
+    #     lazy="joined",
+    # )
+
+    def __repr__(self):
+        return (
+            f'{self.__class__.__name__}('
+            f'id={self.id} '
+            f'tlo_id={self.tlo_id} '
+            f'user_id={self.user_id} '
+            f'commit_message={self.commit_message}'
+            f')'
+        )
