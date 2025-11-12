7d845a9b5c0f891cd1ecb0f0c1e5db5af91987b2
auserw <unnmd88@gmail.com>
2025-11-12 17:15:00 +0300

    add revision: "create OvimPassport table"

--- app/core/models/ovim_passport.py
+++ app/core/models/ovim_passport.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
 from core.models.mixins.integer_pk_id import IntegerIdPkMixin
-from core.models.mixins.timestamp import CreatedAtMixin
+from core.models.mixins.timestamp import CreatedAtMixin, StartAtMixin, FinishedAtMixin
 
 if TYPE_CHECKING:
     from . import User
@@ -15,7 +15,8 @@ if TYPE_CHECKING:
 
 class OvimPassport(
     IntegerIdPkMixin,
-    CreatedAtMixin,
+    StartAtMixin,
+    FinishedAtMixin,
     Base,
 ):
     tlo_id: Mapped[int] = mapped_column(
@@ -26,10 +27,12 @@ class OvimPassport(
     )
     data = mapped_column(
         JSONB,
-        nullable=False,
+        nullable=True,
     )
-    commit_message: Mapped[str] = mapped_column(Text)
-
+    commit_message: Mapped[str] = mapped_column(Text, nullable=True,)
+    editing_now: Mapped[bool] = mapped_column(
+        server_default='true',
+    )
 
     # tlo: Mapped["TrafficLightObject"] = relationship(
     #     lazy="joined",
@@ -43,7 +46,45 @@ class OvimPassport(
             f'{self.__class__.__name__}('
             f'id={self.id} '
             f'tlo_id={self.tlo_id} '
+            f'editing_now={self.editing_now} '
+            f'data={"{...}" if self.data else self.data} '
             f'user_id={self.user_id} '
             f'commit_message={self.commit_message}'
             f')'
         )
+
+
+# class OvimPassport(
+#     IntegerIdPkMixin,
+#     CreatedAtMixin,
+#     Base,
+# ):
+#     tlo_id: Mapped[int] = mapped_column(
+#         ForeignKey('traffic_light_objects.id'),
+#     )
+#     user_id: Mapped[int] = mapped_column(
+#         ForeignKey('users.id'),
+#     )
+#     data = mapped_column(
+#         JSONB,
+#         nullable=False,
+#     )
+#     commit_message: Mapped[str] = mapped_column(Text)
+#
+#
+#     # tlo: Mapped["TrafficLightObject"] = relationship(
+#     #     lazy="joined",
+#     # )
+#     # user: Mapped["User"] = relationship(
+#     #     lazy="joined",
+#     # )
+#
+#     def __repr__(self):
+#         return (
+#             f'{self.__class__.__name__}('
+#             f'id={self.id} '
+#             f'tlo_id={self.tlo_id} '
+#             f'user_id={self.user_id} '
+#             f'commit_message={self.commit_message}'
+#             f')'
+#         )
