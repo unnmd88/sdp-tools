9fdafa9bd7354aab4a1602eec42d4bc641eebdf5
auserw <unnmd88@gmail.com>
2025-11-12 17:16:46 +0300

    implement routes, schemas and crud for passports service

--- app/api/api_v1/passports/routes.py
+++ app/api/api_v1/passports/routes.py
@@ -1,19 +1,13 @@
-from typing import Annotated
 
 from fastapi import (
     APIRouter,
-    Depends,
 )
-from sqlalchemy.ext.asyncio import AsyncSession
+
 from starlette import status
 
 from api.api_v1.passports.crud import OvimPassportsCrud
-from api.api_v1.passports.schemas import CreatePassport
-from api.api_v1.regions.crud import RegionsCrud
-from api.api_v1.tlo.crud import TloCrud
-from api.api_v1.tlo.schemas import TrafficLightCreate, TrafficLightSchema, TrafficLightUpdate
+from api.api_v1.passports.schemas import SavePassport, CapturePassport, SavedPassportSchema
 
-from core.database import db_api
 from core.dependencies import db_session
 
 router = APIRouter(
@@ -33,13 +27,35 @@ async def get_passports_ovim(
     return await OvimPassportsCrud.get_all(session)
 
 
+@router.get(
+    '/ovim/',
+    status_code=status.HTTP_200_OK,
+)
+async def get_passports_ovim(
+    session:  db_session,
+):
+    return await OvimPassportsCrud.get_all(session)
+
+
 @router.post(
     '/ovim/',
     status_code=status.HTTP_201_CREATED,
 )
-async def create_passport_ovim(
+async def capture_editing_passport_ovim(
     session:  db_session,
-    passport: CreatePassport,
+    passport: CapturePassport,
 ):
+    return await OvimPassportsCrud.capture_passport(session, passport)
 
-    return await OvimPassportsCrud.add(session, passport)
+
+@router.patch(
+    '/ovim/',
+    status_code=status.HTTP_202_ACCEPTED,
+    response_model=SavedPassportSchema,
+)
+async def save_passport_ovim(
+    session:  db_session,
+    passport: SavePassport,
+) -> SavedPassportSchema:
+    res = await OvimPassportsCrud.save_passport(session, passport)
+    return SavedPassportSchema.model_validate(res, from_attributes=True)
--- app/api/api_v1/passports/schemas.py
+++ app/api/api_v1/passports/schemas.py
@@ -1,14 +1,38 @@
+from datetime import datetime
 from typing import Annotated
 
 from annotated_types import MaxLen
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, computed_field
 
 
-class CreatePassport(BaseModel):
+class CapturePassport(BaseModel):
     tlo_id: Annotated[int, Field(ge=1)]
     user_id: Annotated[int, Field(ge=1)]
+
+
+class SavePassport(BaseModel):
+    id: Annotated[int, Field(ge=1, exclude=True)]
+    tlo_id: Annotated[int, Field(ge=1, exclude=True)]
+    user_id: Annotated[int, Field(ge=1, exclude=True)]
     data: dict
     commit_message: Annotated[str, MaxLen(255)]
+
+    @computed_field
+    @property
+    def finished_editing_at(self) -> datetime:
+        return datetime.now()
+
+    @computed_field
+    @property
+    def editing_now(self) -> bool:
+        return False
+
+
+class SavedPassportSchema(BaseModel):
+    tlo_id: Annotated[int, Field(ge=1)]
+    # user_id: Annotated[int, Field(ge=1)]
+    finished_editing_at: datetime
+    editing_now: bool
 
 
 class PassportSchema(BaseModel):
