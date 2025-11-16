4e66d3d41622a90d7f1a0a8624c6f1a43f6e3ebd
auserw <unnmd88@gmail.com>
2025-11-16 17:06:17 +0300

    update filters for passport service

--- app/api/api_v1/passports/filters.py
+++ app/api/api_v1/passports/filters.py
@@ -2,6 +2,17 @@ from typing import Annotated
 
 from pydantic import BaseModel, Field
 
+from core.constants import PassportGroupsRoutes
 
-class OvimPassportsFilter(BaseModel):
-    owner_id: Annotated[int, Field(ge=1)]
+
+class PassportGroupIdFilter(BaseModel):
+    group_id: Annotated[int, Field(ge=1)]
+
+
+class PassportGroupNameFilter(BaseModel):
+    group_name_route: Annotated[str, PassportGroupsRoutes]
+
+
+class PassportCurrentFilter(BaseModel):
+    group_id: Annotated[int, Field(ge=1)]
+    tlo_id: Annotated[int, Field(ge=1)]
