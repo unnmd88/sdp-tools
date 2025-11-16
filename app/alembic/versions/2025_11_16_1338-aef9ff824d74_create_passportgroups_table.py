d8d1c4aea817203833c407e184664776ca0eb5d4
auserw <unnmd88@gmail.com>
2025-11-16 17:03:11 +0300

    generate revision: create passport_groups table

--- app/alembic/versions/2025_11_16_1338-aef9ff824d74_create_passportgroups_table.py
+++ app/alembic/versions/2025_11_16_1338-aef9ff824d74_create_passportgroups_table.py
@@ -1,8 +1,8 @@
-"""create PassportsOwner table
+"""create PassportGroups table
 
-Revision ID: 8bf2368dc83c
+Revision ID: aef9ff824d74
 Revises: d0fcb355c83a
-Create Date: 2025-11-12 18:05:08.102841
+Create Date: 2025-11-16 13:38:34.769348
 
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '8bf2368dc83c'
+revision: str = 'aef9ff824d74'
 down_revision: Union[str, Sequence[str], None] = 'd0fcb355c83a'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -21,11 +21,11 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-
     op.create_table(
-        'passports_owners',
+        'passport_groups',
         sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-        sa.Column('owner', sa.String(), nullable=False),
+        sa.Column('group_name', sa.String(), nullable=False),
+        sa.Column('group_name_route', sa.String(length=32), nullable=False),
         sa.Column('description', sa.Text(), server_default='', nullable=False),
         sa.Column(
             'created_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False
@@ -33,12 +33,14 @@ def upgrade() -> None:
         sa.Column(
             'updated_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False
         ),
-        sa.PrimaryKeyConstraint('id', name=op.f('pk_passports_owners')),
-        sa.UniqueConstraint('owner', name=op.f('uq_passports_owners_owner')),
+        sa.PrimaryKeyConstraint('id', name=op.f('pk_passport_groups')),
+        sa.UniqueConstraint('group_name', name=op.f('uq_passport_groups_group_name')),
+        sa.UniqueConstraint(
+            'group_name_route', name=op.f('uq_passport_groups_group_name_route')
+        ),
     )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-
-    op.drop_table('passports_owners')
+    op.drop_table('passport_groups')
