69c7dcdec608dcb49d1503cce1c146893190bdad
auserw <unnmd88@gmail.com>
2025-11-16 17:05:34 +0300

    add dependencies for passport service

--- app/api/api_v1/passports/dependencies.py
+++ app/api/api_v1/passports/dependencies.py
@@ -1,12 +1,14 @@
+from collections.abc import Sequence
+
 from fastapi import HTTPException
 from pydantic import BaseModel
 from starlette import status
 
-from core.database.crud import T
+from core.database.crud import T as T_Model
 
 
 def check_allow_to_save_or_raise_http_exc(
-    editable_db_passport: T,
+    editable_db_passport: T_Model,
     model_to_save: BaseModel,
 ):
     if editable_db_passport is None:
@@ -24,4 +26,40 @@ def check_allow_to_save_or_raise_http_exc(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'Passport editing not found. Please capture passport for editing.',
         )
+    if editable_db_passport.id != model_to_save.id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Invalid passport id',
+        )
+
     return True
+
+
+def passport_group_found_or_404(
+    sequence_group_id: Sequence[T_Model],
+):
+    if len(sequence_group_id) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='No passport found in this group',
+        )
+    elif len(sequence_group_id) > 1:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Founded more than one group',
+        )
+
+
+def current_passport_or_404(
+    current_passport: Sequence[T_Model],
+):
+    if len(current_passport) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='No passport found',
+        )
+    elif len(current_passport) > 1:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Founded more than one passport for this group',
+        )
