88241ba4689a9dc0c410793bac69608025eb973d
auserw <unnmd88@gmail.com>
2025-11-16 13:00:26 +0300

    refactor script

--- start-compose-tests.sh
+++ start-compose-tests.sh
@@ -2,5 +2,5 @@
 
 
 docker compose -f docker-compose.test.yml up --build -d
-docker compose -f docker-compose.test.yml exec -it test_app bash -c "pytest tests/unit -vv"
+docker compose -f docker-compose.test.yml exec -it test_app bash -c "pytest -vv"
 docker compose -f docker-compose.test.yml down --volumes
