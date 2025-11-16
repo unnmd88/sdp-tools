9f5b7a2b51c2deb52b6da2b1071975d30d42b494
auserw <unnmd88@gmail.com>
2025-11-16 13:01:01 +0300

    change host-port for run config

--- app/core/config.py
+++ app/core/config.py
@@ -13,7 +13,7 @@ API_V1_PATH = BASE_DIR / 'api/api_v1/'
 class RunConfig(BaseModel):
     host: str = '0.0.0.0'
     # host: str = '192.168.45.248'
-    port: int = 8001
+    port: int = 8010
     reload: bool = True
 
 
