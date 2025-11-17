e305c80a2a7fb8457c9c876f1bd97c7ff942ff92
auserw <unnmd88@gmail.com>
2025-11-17 13:19:12 +0300

    make revision: create Passport table

---
+++ app/alembic/versions/2025_11_17_1317-162ac5fc7b3b_create_passport_table.py
@@ -0,0 +1,59 @@
+"""create Passport  table
+
+Revision ID: 162ac5fc7b3b
+Revises: 2b8baff9d9d4
+Create Date: 2025-11-17 13:17:58.882997
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = '162ac5fc7b3b'
+down_revision: Union[str, Sequence[str], None] = '2b8baff9d9d4'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        'passports',
+        sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column('tlo_id', sa.Integer(), nullable=False),
+        sa.Column('user_id', sa.Integer(), nullable=False),
+        sa.Column('group_id', sa.Integer(), nullable=False),
+        sa.Column('data', postgresql.JSONB(astext_type=sa.Text()), nullable=True),
+        sa.Column('commit_message', sa.Text(), nullable=True),
+        sa.Column('editing_now', sa.Boolean(), server_default='true', nullable=False),
+        sa.Column(
+            'started_editing_at',
+            sa.DateTime(),
+            server_default=sa.text('now()'),
+            nullable=False,
+        ),
+        sa.Column('finished_editing_at', sa.DateTime(), nullable=True),
+        sa.ForeignKeyConstraint(
+            ['group_id'],
+            ['passport_groups.id'],
+            name=op.f('fk_passports_group_id_passport_groups'),
+        ),
+        sa.ForeignKeyConstraint(
+            ['tlo_id'],
+            ['traffic_light_objects.id'],
+            name=op.f('fk_passports_tlo_id_traffic_light_objects'),
+        ),
+        sa.ForeignKeyConstraint(
+            ['user_id'], ['users.id'], name=op.f('fk_passports_user_id_users')
+        ),
+        sa.PrimaryKeyConstraint('id', name=op.f('pk_passports')),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('passports')
--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -4,12 +4,12 @@ __all__ = (
     'TrafficLightObject',
     'Region',
     'PassportGroup',
-    #'Passport',
+    'Passport',
 )
 
 from .base import Base
 from .tlo import TrafficLightObject
 from .users import User
 from .regions import Region
-#from .passports import Passport
+from .passports import Passport
 from .passport_groups import PassportGroup
