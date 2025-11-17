7fa0809554be44ed41fcc31d2e7fc8949e63d1e9
auserw <unnmd88@gmail.com>
2025-11-17 07:25:26 +0300

    update script for tests

--- start-compose-tests.sh
+++ start-compose-tests.sh
@@ -4,3 +4,5 @@
 docker compose -f docker-compose.test.yml up --build -d
 docker compose -f docker-compose.test.yml exec -it test_app bash -c "pytest -vv"
 docker compose -f docker-compose.test.yml down --volumes
+#docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+docker rmi fastapi-sdp-tools:tests
