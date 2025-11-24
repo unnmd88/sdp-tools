4ea02107ef868dc73d4a1a54e7cf2d4889d25556
auserw <unnmd88@gmail.com>
2025-11-24 19:51:37 +0300

    update

--- app/application/api/api_v1/passports/utils.py
+++ app/application/api/api_v1/passports/utils.py
@@ -11,7 +11,7 @@ def create_current_passport_schema_or_404(
     db_rows_from_mappings: Sequence[RowMapping],
 ):
     if len(db_rows_from_mappings) == 2:
-        first, second = db_rows_from_mappings[0], db_rows_from_mappings[1]
+        first, second = dict(db_rows_from_mappings[0]), dict(db_rows_from_mappings[1])
         if first['editing_now']:
             second.update(editing_now={'editing_now_by_user': first['username']})
             current_passport = second
@@ -23,7 +23,7 @@ def create_current_passport_schema_or_404(
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail=f'Has not current passport, but '
-                f'editing now by user={db_rows_from_mappings[0]["username"]}',
+                       f'editing now by user={db_rows_from_mappings[0]["username"]}',
             )
         return CurrentPassportSchema(**db_rows_from_mappings[0])
 
@@ -37,6 +37,6 @@ def create_current_passport_schema_or_404(
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail=f'Has not current passport, but '
-                f'editing now by user={db_rows_from_mappings[0]["username"]}',
+                       f'editing now by user={db_rows_from_mappings[0]["username"]}',
             )
     raise ValueError('Count rows should be no more than 2')
