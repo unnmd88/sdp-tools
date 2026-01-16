a39f5aa639d278cf00bdc7634bcbb73c5f1ed82c
auserw <unnmd88@gmail.com>
2026-01-16 12:58:36 +0300

    fix

--- app/core/exceptions/contract.py
+++ app/core/exceptions/contract.py
@@ -1,1 +1,9 @@
+from core.exceptions.base import DomainValidationError
 
+
+class ContractViolationError(DomainValidationError):
+    """ Базовый класс ошибки валидации контракта. """
+
+
+class ContractViolationInvariantError(ContractViolationError):
+    """ Ошибка инварианта контракта. """
