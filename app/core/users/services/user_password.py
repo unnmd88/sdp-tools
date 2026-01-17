97ae57d2ff0201031b90216293dd1a2847b8ae5c
auserw <unnmd88@gmail.com>
2026-01-17 20:29:58 +0300

    Refactoring: create UserPasswordService

--- app/core/users/services/user_password.py
+++ app/core/users/services/user_password.py
@@ -4,6 +4,35 @@ import string
 
 import bcrypt
 
+from core.users.business_rules import MIN_LEN_PASSWORD, MAX_LEN_PASSWORD
+
+
+class UserPasswordService:
+
+    @classmethod
+    def hash_password(cls, password: str) -> bytes:
+        return bcrypt.hashpw(
+            password.encode("utf-8"),
+            bcrypt.gensalt(),
+        )
+
+    @classmethod
+    def verify_password(cls, password: str, hashed_password: bytes) -> bool:
+        return bcrypt.checkpw(
+            password=password.encode("utf-8"),
+            hashed_password=hashed_password,
+        )
+
+
+    @classmethod
+    def generate_password(cls) -> str:
+        chars = string.ascii_letters + string.digits + string.punctuation
+        return "".join(
+            secrets.choice(chars) for _ in range(
+                random.randint(MIN_LEN_PASSWORD, MAX_LEN_PASSWORD)
+            )
+        )
+
 
 def hash_password(
     password: str,
